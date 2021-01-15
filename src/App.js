58581bdcf3271e49789ff081cadb8662faabb036
jonnyhak <jonnyhak@gmail.com>
2021-01-14 20:15:53 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
 import Projects from './components/Projects'
 import './App.css';
 
 class App extends Component {
-  render() {
+  
+  state = {
+    projects: []
+  }
 
+  componentDidMount() {
+    fetch('http://localhost:3000/projects')
+      .then(response => {
+        return response.json()
+      })
+      .then(arr => {
+        this.setState({
+          projects: arr
+        })
+      })
+  }
+  
+  render() {
+    console.log(this.state)
     return (
       <div className="App">
-        <Projects />
+        <Switch>
+          <Route path="/projects" render={() => 
+              <Projects projects={this.state.projects}/>
+            }
+          />
+
+        </Switch>
       </div>
     );
   }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,7 @@ class Projects extends Component {
         return (
             <div>
                 <p>Project List</p>
+                
             </div>
         )
     }
